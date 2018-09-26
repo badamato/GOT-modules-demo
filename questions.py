14ff2ae5549933f5e6408f38ad3d7e36b5911da7
Beth Damato <bethadamato@gmail.com>
2018-09-26 10:40:08 -0400

    adjusting file structure to start exercise

--- questions.py
+++ questions.py
@@ -1,2 +1,19 @@
-from modules import characters
+from got import characters  #from the got.py file import the characters dictionary
 
+#How many characters names start with "A"?
+count = 0
+for person in characters:
+    if person['name'] !                     = ''and person['name'][0] == 'A':
+        count = count + 1
+
+print(count)
+
+
+#How many characters names start with "Z"?
+#How many characters are dead?
+#Who has the most titles?
+#How many are Valyrian?
+#What actor plays "Hot Pie" (and don't use IMDB)?
+#How many characters are *not* in the tv show?
+#Produce a list characters with the last name "Targaryen"
+#Create a histogram of the houses (it's the "allegiances" key)
